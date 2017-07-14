e5bd234e66bb0f445d73c415cb242538ce887943
René Nyffenegger <mail@renenyffenegger.ch>
2017-07-14 20:59:00 +0200

    src/bar.h JAVADOC_AUTOBRIEF = YES

---
+++ example/src/bar.h
@@ -0,0 +1,10 @@
+
+
+  
+/** This comment is visible in the *Class List* because
+ *  JAVADOC_AUTOBRIEF is set to YES. Text after
+ *  the first dot is not considered brief with JAVADOC_AUTOBRIEF.
+ * */
+class Bar {
+
+};
